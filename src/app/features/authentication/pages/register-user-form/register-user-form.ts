8f1fd808a33276069f93c2b81f315f5857cfa725
joschonarth <joschonarth@gmail.com>
2026-07-15 20:39:40 -0300

    feat: implement reactive form for register with validation

--- src/app/features/authentication/pages/register-user-form/register-user-form.ts
+++ src/app/features/authentication/pages/register-user-form/register-user-form.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, Field, form, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register-user-form',
-  imports: [],
+  imports: [Field],
   templateUrl: './register-user-form.html',
   styleUrl: './register-user-form.css',
 })
 export class RegisterUserForm {
+  registerModel = signal({
+    name: '',
+    email: '',
+    password: '',
+  });
 
+  registerForm = form(this.registerModel, (fieldPath) => {
+    required(fieldPath.name, { message: 'O nome é obrigatório.' });
+    email(fieldPath.email, { message: 'O e-mail é inválido.' });
+
+    required(fieldPath.password, { message: 'A senha é obrigatória.' });
+    minLength(fieldPath.password, 8, { message: 'A senha deve ter no mínimo 8 caracteres.' });
+  });
 }
