1d9e1361c1e763db75641da7130048ed370f438e
joschonarth <joschonarth@gmail.com>
2026-07-14 22:06:59 -0300

    feat: prevent duplicate calls during login

--- src/app/features/authentication/pages/login-form/login-form.ts
+++ src/app/features/authentication/pages/login-form/login-form.ts
@@ -43,8 +43,8 @@ export class LoginForm {
   loginError = computed(() => setErrorMessage(this.loginResource.error()));
 
   login() {
-    const { email, password } = this.loginForm().value();
+    const credentials = this.loginForm().value();
 
-    this.loginParams.set({ email, password });
+    this.loginParams.set(credentials);
   }
 }
