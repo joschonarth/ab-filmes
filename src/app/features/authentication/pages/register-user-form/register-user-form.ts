0ee27520f59053e2503faa3d05dc33ce355a4e4b
joschonarth <joschonarth@gmail.com>
2026-07-15 21:44:29 -0300

    feat: add success message display for user registration

--- src/app/features/authentication/pages/register-user-form/register-user-form.ts
+++ src/app/features/authentication/pages/register-user-form/register-user-form.ts
@@ -41,6 +41,12 @@ export class RegisterUserForm {
 
   registerError = computed(() => setErrorMessage(this.registerResource.error()));
 
+  successMessage = computed(() => {
+    const SUCCESS_REGISTRATION = this.registerResource.hasValue();
+
+    return SUCCESS_REGISTRATION ? 'Usuário cadastrado com sucesso!' : undefined;
+  });
+
   register() {
     const userInfos = this.registerForm().value();
 
