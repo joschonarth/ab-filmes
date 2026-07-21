7c75cf33ae1bd2f0e19d592e557bee4cf727e53b
joschonarth <joschonarth@gmail.com>
2026-07-21 20:49:31 -0300

    feat: enhance MoviesList component to dynamically display movie details

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { registerLocaleData } from '@angular/common';
+
+import pt from '@angular/common/locales/pt';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/shared/components/movies-list/movies-list.ts
+++ src/app/shared/components/movies-list/movies-list.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { MoviesListResponse } from '../../types/movies-list-response';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './movies-list.html',
   styleUrl: './movies-list.css',
   host: {
@@ -11,5 +12,7 @@ import { MoviesListResponse } from '../../types/movies-list-response';
   },
 })
 export class MoviesList {
+  BASE_PATH = 'http://localhost:3000';
+
   movies = input<MoviesListResponse>([]);
 }
