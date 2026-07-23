411a0204814bd145a51069f38f7e7af1ab25cd96
joschonarth <joschonarth@gmail.com>
2026-07-23 08:23:58 -0300

    feat: implement dynamic star rating system in movie details component

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -1,4 +1,12 @@
-import { Component, inject, input, linkedSignal, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MoviesApi } from '../../services/movies-api';
 import { FavoritesApi } from '../../../../shared/services/favorites-api';
 import { rxResource } from '@angular/core/rxjs-interop';
@@ -34,10 +42,24 @@ export class MovieDetails {
   isFavorite = signal(false);
   currentRating = signal<number | undefined>(undefined);
 
+  starsStatusFilled = computed(() => {
+    const rating = this.currentRating() ?? 0;
+
+    const boolArray = [0, 1, 2, 3, 4].map((index) => index < rating);
+
+    return boolArray;
+  });
+
   toggleFavorite() {
     this.isFavorite.update((value) => !value);
     console.log(`Filme agora é favorito: ${this.isFavorite()}`);
   }
 
-  updateRating(newRating: number) {}
+  updateRating(newRating: number) {
+    if (newRating === this.currentRating()) {
+      this.currentRating.set(0);
+    } else {
+      this.currentRating.set(newRating);
+    }
+  }
 }
